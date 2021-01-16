80202bc1121191e7fb6d011fb2fd3d43b4664292
Bill Winkler <bill.winkler@gmail.com>
2021-01-16 11:18:17 -0600

    begin 0.1.2 snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-translate "0.1.1"
+(defproject google-translate "0.1.2-SNAPSHOT"
   :description "A wrapper for the Google Translate API"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
