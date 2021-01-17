4f0a57e5294c3b65ef0499e0dbe96a08f943735c
Bill Winkler <bill.winkler@gmail.com>
2021-01-17 10:54:21 -0600

    version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-translate "0.1.1"
+(defproject google-translate "0.1.2"
   :description "A wrapper for the Google Translate API"
   :url "https://github.com/billwinkler/google-translate"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
